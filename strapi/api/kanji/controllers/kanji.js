e40eb0b0c0c01b98de0314646f263fa7f81be443
scm-naingaunglwin <scm.naingaunglwin@gmail.com>
2021-07-15 17:56:58 +0630

    feature/quiz_ap implements quiz api

--- strapi/api/kanji/controllers/kanji.js
+++ strapi/api/kanji/controllers/kanji.js
@@ -1,6 +1,7 @@
 'use strict';
-const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+const { sanitizeEntity } = require('strapi-utils');
 const _ = require('lodash');
+const { isNil, conforms } = require('lodash');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -8,28 +9,106 @@ const _ = require('lodash');
  */
 
 module.exports = {
-    async importKanji(ctx) {
-        const words = ctx.request.body;
-        let entities = [];
-        try {
-          await Promise.all(words.map(async (word) => {
-            let isDuplicated = false;
-            let messages = 'duplicate words : ';
-            if (await strapi.services.kanji.isDuplicated(word.kanji)) {
-              isDuplicated = true;
-              messages = messages + `${word.kanji}`;
-            }
-            if (isDuplicated) {
-              return ctx.response.conflict(messages);
-            }
-            let entity = await strapi.services.kanji.create(word);
-            entities.push(entity);
-          }));
-          
-        } catch (err) {
-          console.error(err.message);
-          return ctx.response.badRequest(err.message);
+  async importKanji(ctx) {
+    const words = ctx.request.body;
+    let entities = [];
+    try {
+      await Promise.all(words.map(async (word) => {
+        let isDuplicated = false;
+        let messages = 'duplicate words : ';
+        if (await strapi.services.kanji.isDuplicated(word.kanji)) {
+          isDuplicated = true;
+          messages = messages + `${word.kanji}`;
         }
-        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.kanji }));
-      },
+        if (isDuplicated) {
+          return ctx.response.conflict(messages);
+        }
+        let entity = await strapi.services.kanji.create(word);
+        entities.push(entity);
+      }));
+
+    } catch (err) {
+      console.error(err.message);
+      return ctx.response.badRequest(err.message);
+    }
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.kanji }));
+  },
+
+  async quizAnswerWithMeaning(ctx) {
+    try {
+      const quizs = [];
+      const entities = await strapi.services.kanji.find({ _limit: -1 });
+      if (entities.length == 0) {
+        return ctx.send({ message: 'No data in kanji table' }, 400);
+      }
+      const meaning_list = _.map(entities, entity => {
+        return entity.meaning;
+      });
+      const randomMeaning = strapi.services.kanji.getRandom(entities, 30);
+      if (isNil(randomMeaning)) {
+        return ctx.send({ message: 'Data is null or underfined value' }, 404);
+      }
+      if (randomMeaning.length == 0) {
+        return ctx.send({ message: 'No kanji words found' }, 404);
+      }
+      await Promise.all(randomMeaning.map(word => {
+        let meanings = _.cloneDeep(meaning_list);
+        meanings = _.filter(meaning_list, m => m !== word.meaning);
+        let answer_list = strapi.services.kanji.getRandom(meanings, 5)
+        answer_list.push(word.meaning);
+        answer_list = strapi.services.kanji.shuffle(answer_list);
+        const quiz = {
+          kanji: word.kanji,
+          correct: word.meaning,
+          answer_list
+        }
+        console.log(quiz);
+        quizs.push(quiz);
+      }))
+      return quizs;
+    }
+    catch(error) {
+      console.log('=========== Error in quiz ========');
+      console.error(error);
+    }
+  },
+
+  async quizAnswerWithKanji(ctx) {
+    try {
+      const quizs = [];
+      const entities = await strapi.services.kanji.find({ _limit: -1 });
+      if (entities.length == 0) {
+        return ctx.send({ message: 'No data in kanji table' }, 400);
+      }
+      const kanji_list = _.map(entities, entity => {
+        return entity.kanji;
+      });
+      const randomMeaning = strapi.services.kanji.getRandom(entities, 30);
+      if (isNil(randomMeaning)) {
+        return ctx.send({ message: 'Data is null or underfined value' }, 404);
+      }
+      if (randomMeaning.length == 0) {
+        return ctx.send({ message: 'No meaning found' }, 404);
+      }
+      await Promise.all(randomMeaning.map(word => {
+        let kanjis = _.cloneDeep(kanji_list);
+        kanjis = _.filter(kanji_list, m => m !== word.kanji);
+        let answer_list = strapi.services.kanji.getRandom(kanjis, 5)
+        answer_list.push(word.kanji);
+        answer_list = strapi.services.kanji.shuffle(answer_list);
+        const quiz = {
+          meaning: word.meaning,
+          correct: word.kanji,
+          answer_list
+        }
+        console.log(quiz);
+        quizs.push(quiz);
+      }))
+      return quizs;
+    }
+    catch(error) {
+      console.log('=========== Error in quiz ========');
+      console.error(error);
+    }
+  }
 };
--- strapi/api/kanji/services/kanji.js
+++ strapi/api/kanji/services/kanji.js
@@ -6,7 +6,39 @@
  */
 
 module.exports = {
-    isDuplicated: async (word) => {
-        return await strapi.services.kanji.count({ kanji: word }) > 0;
+  isDuplicated: async (word) => {
+    return await strapi.services.kanji.count({ kanji: word }) > 0;
+  },
+
+  getRandom: (arr, n) => {
+    var result = new Array(n),
+      len = arr.length,
+      taken = new Array(len);
+    if (n > len)
+      throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+      var x = Math.floor(Math.random() * len);
+      result[n] = arr[x in taken ? taken[x] : x];
+      taken[x] = --len in taken ? taken[len] : len;
     }
+    return result;
+  },
+
+  shuffle: (array) => {
+    var currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  }
 };
