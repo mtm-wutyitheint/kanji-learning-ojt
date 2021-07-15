bf0a65f17b2054ea776f4e2036c0170f846ca01f
scm-wutyitheint <scm.dott.developer10@gmail.com>
2021-07-15 10:47:06 +0630

    fix error sanitizeEntity

--- strapi/api/kanji/controllers/kanji.js
+++ strapi/api/kanji/controllers/kanji.js
@@ -1,4 +1,6 @@
 'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+const _ = require('lodash');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -12,19 +14,18 @@ module.exports = {
         try {
           await Promise.all(words.map(async (word) => {
             let isDuplicated = false;
-            let messages = 'duplicate words.:';
+            let messages = 'duplicate words : ';
             if (await strapi.services.kanji.isDuplicated(word.kanji)) {
               isDuplicated = true;
-              messages = messages + `${word.kanji}\n`;
+              messages = messages + `${word.kanji}`;
             }
             if (isDuplicated) {
               return ctx.response.conflict(messages);
             }
-          }));
-          await Promise.all(words.map(async (word) => {
             let entity = await strapi.services.kanji.create(word);
             entities.push(entity);
           }));
+          
         } catch (err) {
           console.error(err.message);
           return ctx.response.badRequest(err.message);
