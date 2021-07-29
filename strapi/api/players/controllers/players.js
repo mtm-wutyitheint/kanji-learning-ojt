7b46a8d1da15bc8568d48cc45289e6b2b7d41fd6
scm-naingaunglwin <scm.naingaunglwin@gmail.com>
2021-07-29 11:57:27 +0630

    player api

---
+++ strapi/api/players/controllers/players.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  async player_login(ctx) {
+    const player = ctx.request.body;
+    if (!('name' in player) || !('password' in player)) {
+      return ctx.send({ message: 'invalid data' }, 400);
+    }
+    const isExist = await strapi.services.players.findOne(player);
+    if (isExist === null) {
+      return ctx.send({status: "login fail!!! username or password invalid"}, 400);
+    }
+    return {
+      status: 'success',
+      name: isExist.name,
+    }
+  }
+};
