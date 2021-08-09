053489443f1ec652af15413866f8a5ec7981c076
scm-wutyitheint <scm.dott.developer10@gmail.com>
2021-08-09 11:01:16 +0630

    import multiple images

--- import-img/main.py
+++ import-img/main.py
@@ -1,16 +1,32 @@
-import requests 
+import requests
+import os
 
 
 def InsertBlob(filepath):
-    with open(filepath, "rb") as File:
-        files = {'files': ('12.png', open(filepath, 'rb'), 'image', {'uri': ''})}
-        response = requests.post('http://localhost:1337/upload', files=files).json()
+    for path in os.listdir(filepath):
+        data_path = os.path.join(filepath, path)
+        print(path)
+        URL = 'http://localhost:1337/kanjis'
+        PARAMS = {'kanji': path}
+        r = requests.get(url=URL, params=PARAMS).json()
+
+        img_path = os.path.join(filepath, path, os.listdir(data_path)[0])
+        img_path02 = os.path.join(filepath, path, os.listdir(data_path)[1])
+
+        file01 = {'files': (f'{path}1.png', open(img_path, 'rb'), 'image', {'uri': ''})}
+        response01 = requests.post('http://localhost:1337/upload', files=file01).json()
+
+        file02 = {'files': (f'{path}2.png', open(img_path02, 'rb'), 'image', {'uri': ''})}
+        response02 = requests.post('http://localhost:1337/upload', files=file02).json()
+
         payload = {
-            "logoPicture": response[0]['id']
+            "logoPicture": response01[0]['id'],
+            "examplePicture": response02[0]['id']
         }
-        response = requests.put('http://localhost:1337/kanjis/229', json=payload)
+        imgid = r[0]['id']
+        response = requests.put(f'http://localhost:1337/kanjis/{imgid}', json=payload)
         print(response.status_code)
 
 
-UserFilePath = input("Enter File Path: ")
+UserFilePath = input("Enter File Path: ")  # "D:\JPLEarningOJT\japanese_learning_game\import-img\imgs"
 InsertBlob(UserFilePath)
